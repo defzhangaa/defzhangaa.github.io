261470776f3fc598f6eeec8e594b3eac8de7165e
Jiaxu Zhang <zhangjiaxu402@163.com>
2024-04-03 22:27:48 +0800

    Update time

--- js/index.js
+++ js/index.js
@@ -19,20 +19,24 @@ function clock() {
       let nowHours = date.getHours() // 获取当前时间的小时数
       let nowMinutes = date.getMinutes() // 获取当前时间的分钟数
       let nowSecond = date.getSeconds() // 获取当前时间的秒数
-      nowHours = nowHours < 10 ? '0' + nowHours : nowHours // 如果当前小时数小于10，则在前面添加0
-      nowMinutes = nowMinutes < 10 ? '0' + nowMinutes : nowMinutes // 如果当前秒数小于10，则在前面添加0
-      nowSecond = nowSecond < 10 ? '0' + nowSecond : nowSecond // 如果当前秒数小于10，则在前面添加0
-      // 将当前时间显示在.oneDay的div容器中
-      document.querySelector('.oneDay').innerHTML = `${nowHours}:${nowMinutes}:${nowSecond}&nbsp UTC+08：00`
-    }, 1000) // 设置定时器的时间间隔为1000毫秒
- 
-    setInterval(function () { // 创建另一个定时器
-      const date = new Date() // 获取当前时间并存储在变量date中
       const nowYear = date.getFullYear() // Get year
       const nowMonth = date.getMonth() // 获取当前月份
       const nowDate = date.getDate() // 获取当前日期
       const nowDay = date.getDay() // 获取当前星期几对应的数字
-      // 将当前日期和星期几以及月份显示在.month的p容器中
-      document.querySelector('.month').innerHTML = `${nowMonth + 1}/${nowDate}/${nowYear}/&nbsp${day[nowDay-1]}day`
+      nowHours = nowHours < 10 ? '0' + nowHours : nowHours // 如果当前小时数小于10，则在前面添加0
+      nowMinutes = nowMinutes < 10 ? '0' + nowMinutes : nowMinutes // 如果当前秒数小于10，则在前面添加0
+      nowSecond = nowSecond < 10 ? '0' + nowSecond : nowSecond // 如果当前秒数小于10，则在前面添加0
+      // 将当前时间显示在.oneDay的div容器中
+      document.querySelector('.oneDay').innerHTML = `${nowHours}:${nowMinutes}:${nowSecond}&nbsp UTC+08:00 &nbsp ${nowMonth + 1}/${nowDate}/${nowYear}/&nbsp${day[nowDay-1]}day`
     }, 1000) // 设置定时器的时间间隔为1000毫秒
+ 
+    // setInterval(function () { // 创建另一个定时器
+    //   const date = new Date() // 获取当前时间并存储在变量date中
+    //   const nowYear = date.getFullYear() // Get year
+    //   const nowMonth = date.getMonth() // 获取当前月份
+    //   const nowDate = date.getDate() // 获取当前日期
+    //   const nowDay = date.getDay() // 获取当前星期几对应的数字
+    //   // 将当前日期和星期几以及月份显示在.month的p容器中
+    //   document.querySelector('.month').innerHTML = `${nowMonth + 1}/${nowDate}/${nowYear}/&nbsp${day[nowDay-1]}day`
+    // }, 1000) // 设置定时器的时间间隔为1000毫秒
 }
